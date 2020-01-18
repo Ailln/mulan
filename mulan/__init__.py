3ca5130defab83fb1ca4d260df99e1251f988fbd
Ailln <kinggreenhall@gmail.com>
2020-01-18 21:20:16 +0800

    add mulanshi.

--- mulan/__init__.py
+++ mulan/__init__.py
@@ -1,9 +1,11 @@
 from . import shell
 from . import version
+from .util import shi
 
 __version__ = version.VERSION
 
 __all__ = [
     "shell",
-    "version"
+    "version",
+    "shi"
 ]
--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup
 from setuptools import find_packages
 
-# change this.
-NAME = "your package name"
-AUTHOR = "your name"
-EMAIL = "your mail"
-URL = "your project url"
-LICENSE = "your license"
-DESCRIPTION = "your project description"
+NAME = "mulan"
+AUTHOR = "Ailln"
+EMAIL = "kinggreenhall@gmail.com"
+URL = "https://github.com/Ailln/mulan"
+LICENSE = "MIT License"
+DESCRIPTION = "木兰诗复读机"
 
 if __name__ == "__main__":
     setup(
         name=NAME,
-        version="0.0.1",
+        version="0.0.3",
         author=AUTHOR,
         author_email=EMAIL,
         url=URL,
@@ -23,21 +22,18 @@ if __name__ == "__main__":
         install_requires=open("./requirements.txt", "r").read().splitlines(),
         long_description=open("./README.md", "r").read(),
         long_description_content_type='text/markdown',
-        # change package_name to your package name.
         entry_points={
             "console_scripts": [
-                "package_name=package_name.shell:run"
+                "mulan=mulan.shell:run"
             ]
         },
         package_data={
-            # change package_name to your package name.
-            "package_name": ["src/*.txt"]
+            "mulan": ["src/*.txt"]
         },
         zip_safe=True,
         classifiers=[
             "Programming Language :: Python :: 3",
-            # change $license to your license.
-            "License :: OSI Approved :: $license",
+            "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
         ],
         python_requires=">=3.6"
